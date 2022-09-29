523b4fed9ad228bfe752a2c117142315e4ea5c1a
Aya Zayed <ayazayed@std.mans.edu.eg>
2022-09-29 18:14:12 +0200

    basic login logout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./css/App.css";
+import LoginButton from "./LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import Logout from "./Logout";
 
 function App() {
-  return <div className="App"></div>;
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="App">{!isAuthenticated ? <LoginButton /> : <Logout />}</div>
+  );
 }
 
 export default App;
